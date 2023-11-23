bfe0c231f0ad8751240202c124f77eecda44a0de
Laiba Saeed <laibasaeed538@gmail.com>
2023-11-23 19:46:43 +0500

    Navbar, register and login components.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,22 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Chat from "./containers/Chat";
 import Register from "./containers/Register";
 import Login from "./containers/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import NavBar from "./components/NAvBar";
+
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Chat />}></Route>
-        <Route path="/register" element={<Register />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="*" element={<Navigate to="/" />}></Route>
-      </Routes>
+      <NavBar />
+      <Container>
+        <Routes className="text-secondary">
+          <Route path="/" element={<Chat />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="*" element={<Navigate to="/" />}></Route>
+        </Routes>
+      </Container>
     </>
   );
 }
--- client/src/containers/Register.jsx
+++ client/src/containers/Register.jsx
@@ -1,5 +1,36 @@
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+
 const Register = () => {
-  return <>Register</>;
+  return (
+    <>
+      <Form>
+        <Row
+          style={{
+            height: "100vh",
+            justifyContent: "center",
+            paddingTop: "7%",
+          }}
+        >
+          <Col xs={6}>
+            <Stack gap={3}>
+              <h2>Register</h2>
+
+              <Form.Control type="text" placeholder="Name" />
+              <Form.Control type="email" placeholder="Email" />
+              <Form.Control type="password" placeholder="Password" />
+              <button className="blue-button" type="submit">
+                Register
+              </button>
+
+              <Alert variant="danger">
+                <p>An error occured</p>
+              </Alert>
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
 };
 
 export default Register;
