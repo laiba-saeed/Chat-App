a30f333d0430502bc39a1a14a355298402809f84
Laiba Saeed <laibasaeed538@gmail.com>
2023-11-26 12:23:57 +0500

    Used Context API

--- client/src/containers/Register.jsx
+++ client/src/containers/Register.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
 import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+import { AuthContext } from "../context/AuthContext";
 
 const Register = () => {
+  const { registerInfo, updateRegisterInfo } = useContext(AuthContext);
+
   return (
     <>
       <Form>
@@ -15,12 +19,36 @@ const Register = () => {
             <Stack gap={3}>
               <h2>Register</h2>
 
-              <Form.Control type="text" placeholder="Name" />
-              <Form.Control type="email" placeholder="Email" />
-              <Form.Control type="password" placeholder="Password" />
-              <button className="blue-button" type="submit">
+              <Form.Control
+                type="text"
+                placeholder="Name"
+                onChange={(e) => {
+                  updateRegisterInfo({ ...registerInfo, name: e.target.value });
+                }}
+              />
+              <Form.Control
+                type="email"
+                placeholder="Email"
+                onChange={(e) => {
+                  updateRegisterInfo({
+                    ...registerInfo,
+                    email: e.target.value,
+                  });
+                }}
+              />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                onChange={(e) => {
+                  updateRegisterInfo({
+                    ...registerInfo,
+                    password: e.target.value,
+                  });
+                }}
+              />
+              <Button variant="primary" type="submit">
                 Register
-              </button>
+              </Button>
 
               <Alert variant="danger">
                 <p>An error occured</p>
