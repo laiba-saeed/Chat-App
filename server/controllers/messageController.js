c9e5622e9398b2abf63526a1f37e7bc50b2d37d0
Laiba Saeed <laibasaeed538@gmail.com>
2023-12-02 20:04:59 +0500

    Messages APIs

---
+++ server/controllers/messageController.js
@@ -0,0 +1,24 @@
+const messageModel = require("../models/messageModel");
+
+exports.createMessage = async (req, res) => {
+  const { userId, chatId, text } = req.body;
+  const message = new messageModel({ userId, chatId, text });
+  try {
+    const response = await message.save();
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+exports.getMessages = async (req, res) => {
+  const { chatId } = req.params;
+  try {
+    const messages = await messageModel.find({ chatId });
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
