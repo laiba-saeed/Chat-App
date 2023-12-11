01710089baa3602d79a350cab4389309f9fe0c8d
Laiba Saeed <laibasaeed538@gmail.com>
2023-12-11 11:09:47 +0500

    Create chats

--- client/src/containers/Chat.jsx
+++ client/src/containers/Chat.jsx
@@ -3,6 +3,7 @@ import { Container, Stack } from "react-bootstrap";
 import { ChatContext } from "../context/chatContext";
 import { AuthContext } from "../context/AuthContext";
 import UserChat from "../components/chat/userChat";
+import PotentialChats from "../components/chat/potentialChats";
 
 const Chat = () => {
   const { user } = useContext(AuthContext);
@@ -10,6 +11,7 @@ const Chat = () => {
     useContext(ChatContext);
   return (
     <Container>
+      <PotentialChats />
       {userChats?.length < 1 ? null : (
         <Stack direction="horizontal" gap={4} className="align-items-start">
           <Stack className="message-box flex-grow-0 pe-3" gap={3}>
--- client/src/context/chatContext.jsx
+++ client/src/context/chatContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useCallback } from "react";
 import { baseUrl, getRequest, postRequest } from "../utils/services";
 
 export const ChatContext = createContext();
@@ -7,7 +7,40 @@ export const ChatContextProvider = ({ children, user }) => {
   const [userChats, setUserChats] = useState(null);
   const [isUserChatsLoading, setIsUserChatsLoading] = useState(false);
   const [userChatsError, setuserChatsError] = useState(null);
+  const [potentialChats, setPotentialChats] = useState([]);
 
+  //get users whom we can start a chat with
+  useEffect(() => {
+    const getUsers = async () => {
+      const response = await getRequest(`${baseUrl}/users`);
+
+      if (response.error) {
+        return console.log("Error fetching users", response);
+      }
+
+      const pChats = response.filter((u) => {
+        let isChatCreated = false;
+
+        //we donot have to list currently loggedIn user
+        if (user?._id === u?._id) return false;
+
+        //if chat is created with the loggedIn user or not
+        if (userChats) {
+          isChatCreated = userChats?.some((chat) => {
+            return chat.members[0] === u._id || chat.members[0] === u._id;
+          });
+        }
+
+        return !isChatCreated;
+      });
+
+      setPotentialChats(pChats);
+    };
+
+    getUsers();
+  }, [userChats]);
+
+  //get a user chats
   useEffect(() => {
     const getUserChats = async () => {
       if (user?._id) {
@@ -29,9 +62,32 @@ export const ChatContextProvider = ({ children, user }) => {
     getUserChats();
   }, [user?._id]);
 
+  //function to create chat
+  const createChat = useCallback(async (firstId, secondId) => {
+    const response = await postRequest(
+      `${baseUrl}/chats`,
+      JSON.stringify({
+        firstId,
+        secondId,
+      })
+    );
+
+    if (response.error) {
+      return console.log("Error creating chat", response);
+    }
+
+    setUserChats((prev) => [...prev, response]);
+  }, []);
+
   return (
     <ChatContext.Provider
-      value={{ userChats, isUserChatsLoading, userChatsError }}
+      value={{
+        userChats,
+        isUserChatsLoading,
+        userChatsError,
+        potentialChats,
+        createChat,
+      }}
     >
       {children}
     </ChatContext.Provider>
